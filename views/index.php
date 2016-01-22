ae92d18af3262291cb53a8cacdb2df7833f45cfb
Andrew Eremenko <feanir@me.com>
2016-01-22 18:01:58 +0300

    added some html

--- views/index.php
+++ views/index.php
@@ -1,1 +1,72 @@
-index
+<div class="section no-pad-bot" id="index-banner">
+  <div class="container container-video">
+    <div class="video-bg-wrap">
+      <div class="video-overlay"></div>
+      <video class="video-bg lazy-hidden" loop="" autoplay="" data-ipad="false" data-apple-source="http://rfcdn.com/retrofuzz/ipad/header-final-ipad-201502161045.mp4" poster="http://retrofuzz.com/assets/images/video-stills/intro_for_ipad.jpg" style="background-image: url(http://retrofuzz.com/undefined);">
+        <source data-src="http://rfcdn.com/retrofuzz/header-v5-h264-2.mp4" type="video/mp4" src="http://rfcdn.com/retrofuzz/header-v5-h264-2.mp4">
+        <source data-src="http://rfcdn.com/retrofuzz/header-v5-ogg-1.ogg" type="video/ogg" src="http://rfcdn.com/retrofuzz/header-v5-ogg-1.ogg">
+      </video>
+    </div>
+    <div class="start">
+      <h1 class="header center white-text">Лови момент</h1>
+      <div class="row center">
+        <h5 class="header col s12 light white-text">Какой-нибудь умный текст</h5>
+      </div>
+      <div class="row center">
+        <a href="#" id="download-button" class="btn-large waves-effect waves-light red">Начать</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="section">
+  <br><br>
+    <!--   Icon Section   -->
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="card small">
+            <div class="card-image">
+              <img src="http://www.wareable.com/media/images/2014/10/aaaafortheawesome-1412334292-8otB-column-width-inline.jpg">
+              <span class="card-title">Качели</span>
+            </div>
+            <div class="card-content">
+              <p>Иван Иванов</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Посмотреть</a>
+            </div>
+          </div>
+      </div>
+      <div class="col s12 m4">
+        <div class="card small">
+            <div class="card-image">
+              <img src="http://www.wareable.com/media/images/2014/10/windlands-oculus-rift-1-1412334393-0NUf-column-width-inline.jpg">
+              <span class="card-title">Хоррор</span>
+            </div>
+            <div class="card-content">
+              <p>Петр Петров</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Посмотреть</a>
+            </div>
+          </div>
+      </div>
+      <div class="col s12 m4">
+        <div class="card small">
+            <div class="card-image">
+              <img src="http://www.wareable.com/media/images/2014/10/vanguard-v-4-1-1412334418-pFCs-column-width-inline.jpg">
+              <span class="card-title">Полет</span>
+            </div>
+            <div class="card-content">
+              <p>Константин Константинопольский</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Посмотреть</a>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -1,11 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Pure moment</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="/fancybox/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
 </head>
 <body>
-123
-<?=$content?>
+  <nav class="black lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <div id="logo-container" class="brand-logo">
+          <img src="logo.svg" width="60" alt="">
+      </div>
+    </div>
+  </nav>
+
+  <?=$content?>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="/fancybox/jquery.fancybox.pack.js?v=2.1.5"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+  <script src="/js/materialize.js"></script>
+  <script src="/js/init.js"></script>
 </body>
 </html>
